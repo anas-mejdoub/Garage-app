55ca5ebbac3942051952eaec42326e2da678face
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-05-24 12:07:47 +0100

    dash

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,4 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+
 import { Head } from '@inertiajs/react';
 import { InertiaLink } from '@inertiajs/inertia-react';
 function Sidebar() {
@@ -23,14 +26,23 @@ function Sidebar() {
 }
 
 export default function Dashboard({ auth }) {
+    const [isSidebarVisible, setSidebarVisible] = useState(true);
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<><h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2></>}
+            header={
+                <>
+                    <h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+                    {/* <FaBars 
+                        onClick={() => setSidebarVisible(!isSidebarVisible)}
+                        className="ml-4 text-blue-500 hover:text-blue-700 cursor-pointer"
+                    /> */}
+                </>
+            }
         >
             <Head title="Dashboard" />
             <div className="flex">
-                <Sidebar />
+            <Sidebar/>
                 <div className="py-12 flex-grow">
                     <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">

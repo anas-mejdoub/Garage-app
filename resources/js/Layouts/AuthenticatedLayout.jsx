24bc3118abcb380c55694305da081274d0a80ffe
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-05-24 11:38:48 +0100

    logo

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -16,7 +16,8 @@ export default function Authenticated({ user, header, children }) {
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <img src="/Classic-Garage-Logo-Design-by-jdrdesigns.jpg" alt="" style={{ width: '100px', height: '80px' }}/>
+                                    {/* <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" /> */}
                                 </Link>
                             </div>
 

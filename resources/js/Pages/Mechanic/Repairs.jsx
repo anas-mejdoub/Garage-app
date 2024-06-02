fbee884c5413de7a19815d76cfd50424ccb34b34
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-06-02 14:44:44 +0100

    working on page

--- resources/js/Pages/Mechanic/Repairs.jsx
+++ resources/js/Pages/Mechanic/Repairs.jsx
@@ -4,7 +4,7 @@ import { AiFillCheckCircle, AiFillCloseCircle, AiOutlineCar, AiOutlineSync } fro
 import Modal from 'react-modal';
 import NavBar from './Navbar';
 
-const MechanicRepairs = ({ repairs , auth, notifications}) => {
+const MechanicRepairs = ({ repairs, auth, notifications }) => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [newStatus, setNewStatus] = useState("completed");
     const [currentRepair, setCurrentRepair] = useState(null);
@@ -27,105 +27,102 @@ const MechanicRepairs = ({ repairs , auth, notifications}) => {
     return (
         <div>
             <NavBar auth={auth.user} notifications={notifications} />
-        <div className="p-6 bg-gray-700 flex flex-col gap-4 items-center" style={{ background: '#161D32', minHeight: '100vh' }}>
-            <h1 className="text-5xl font-semibold text-gray-300 leading-tight mb-6 flex items-center">
-                <AiOutlineCar className="mr-2" /> Repairs assigned to me
-            </h1>
-            <div className="max-w-2xl w-[60em] p-6 bg-gray-900 text-gray-400 rounded-lg min-w-full">
-                <table className="table-auto w-full border-collapse min-w-full">
-                    <thead>
-                        <tr className="hover:bg-gray-700 transition-colors duration-300">
-                            <th className="px-4 py-2 border-gray-300 text-center">Repair ID</th>
-                            <th className="px-4 py-2 border-gray-300 text-center">Title</th>
-                            <th className="px-4 py-2 border-gray-300 text-center">Description</th>
-                            <th className="px-4 py-2 border-gray-300 text-center">Status</th>
-                            <th className="px-4 py-2 border-gray-300 text-center">Change Status</th>
-                            <th className="px-4 py-2 border-gray-300 text-center">Working on</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {repairs.map((repair) => (
-                            <tr key={repair.id} className="hover:bg-gray-700 transition-colors duration-300">
-                                <td className="border-gray-300 px-4 py-2 text-center">{repair.id}</td>
-                                <td className="border-gray-300 px-4 py-2 text-center">{repair.title}</td>
-                                <td className="border-gray-300 px-4 py-2 text-center">{repair.description}</td>
-                                <td className="border-gray-300 px-4 py-2 text-center">
-                                    {repair.status === 'completed' ? (
-                                        <div className="text-green-500 flex items-center justify-center">
-                                            <AiFillCheckCircle className="mr-1" /> Completed
-                                        </div>
-                                    ) : (
-                                        <div className="text-red-500 flex items-center justify-center">
-                                            <AiFillCloseCircle className="mr-1" /> Pending
-                                        </div>
-                                    )}
-                                </td>
-                                <td className=" flex items-center justify-center">
-                                    <button
-                                        onClick={() => openModal(repair)}
-                                        className="bg-blue-500 text-white rounded px-2 py-1 flex items-center justify-center"
-                                    >
-                                        <AiOutlineSync className="mr-1" /> Change Status
-                                    </button>
-                                </td>
-                                <td className="border-gray-300 px-4 py-2 text-center">
-                                    {repair.status !== 'completed' && (
-                                        <button
-                                        onClick={() => Inertia.get(`/mechanic/repairs/working/${repair.id}`)}
-                                        className="bg-yellow-500 text-white rounded px-2 py-1 flex items-center justify-center mx-auto"
-                                        >
-                                            <AiOutlineSync className="mr-1" /> Working on
-                                        </button>
-                                    )}
-                                    {
-                                        repair.status === 'completed' && (
-                                            <button
-                                            disabled={true}
-                                            onClick={() => Inertia.get(`/mechanic/repairs/working/${repair.id}`)}
-                                            className="bg-yellow-500 disabled:cursor-not-allowed text-white rounded px-2 py-1 flex items-center justify-center mx-auto"
-                                        >
-                                            <AiOutlineSync className="mr-1" /> Working on
-                                        </button>
-                                        )   
-                                    }
-                                </td>
+            <div className="p-6 bg-gray-700 flex flex-col gap-4 items-center" style={{ background: '#161D32', minHeight: '100vh' }}>
+                <h1 className="text-5xl font-semibold text-gray-300 leading-tight mb-6 flex items-center">
+                    <AiOutlineCar className="mr-2" /> Repairs assigned to me
+                </h1>
+                <div className="max-w-2xl w-[60em] p-6 bg-gray-900 text-gray-400 rounded-lg min-w-full">
+                    <table className="table-auto w-full border-collapse min-w-full">
+                        <thead>
+                            <tr className="hover:bg-gray-700 transition-colors duration-300">
+                                <th className="px-4 py-2 border-gray-300 text-center">Repair ID</th>
+                                <th className="px-4 py-2 border-gray-300 text-center">Title</th>
+                                <th className="px-4 py-2 border-gray-300 text-center">Description</th>
+                                <th className="px-4 py-2 border-gray-300 text-center">Status</th>
+                                <th className="px-4 py-2 border-gray-300 text-center">Change Status</th>
+                                <th className="px-4 py-2 border-gray-300 text-center">Working on</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                contentLabel="Change Status Modal"
-                className="m-auto w-11/12 md:w-1/2 lg:w-1/3 border border-gray-300 shadow-lg p-6 rounded-md bg-gray-900"
-                overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex"
+                        </thead>
+                        <tbody>
+                            {repairs.map((repair) => (
+                                <tr key={repair.id} className="hover:bg-gray-700 transition-colors duration-300">
+                                    <td className="border-gray-300 px-4 py-2 text-center">{repair.id}</td>
+                                    <td className="border-gray-300 px-4 py-2 text-center">{repair.title}</td>
+                                    <td className="border-gray-300 px-4 py-2 text-center">{repair.description}</td>
+                                    <td className="border-gray-300 px-4 py-2 text-center">
+                                        {repair.status === 'completed' ? (
+                                            <div className="text-green-500 flex items-center justify-center">
+                                                <AiFillCheckCircle className="mr-1" /> Completed
+                                            </div>
+                                        ) : (
+                                            <div className="text-red-500 flex items-center justify-center">
+                                                <AiFillCloseCircle className="mr-1" /> Pending
+                                            </div>
+                                        )}
+                                    </td>
+                                    <td className="border-gray-300 px-4 py-2 text-center">
+                                        <button
+                                            onClick={() => openModal(repair)}
+                                            className="bg-blue-500 text-white rounded px-2 py-1 flex items-center justify-center"
+                                        >
+                                            <AiOutlineSync className="mr-1" /> Change Status
+                                        </button>
+                                    </td>
+                                    <td className="border-gray-300 px-4 py-2 text-center">
+                                        {repair.status !== 'completed' ? (
+                                            <button
+                                                onClick={() => Inertia.get(`/mechanic/repairs/working/${repair.id}`)}
+                                                className="bg-yellow-500 text-white rounded px-2 py-1 flex items-center justify-center mx-auto"
+                                            >
+                                                <AiOutlineSync className="mr-1" /> Working on
+                                            </button>
+                                        ) : (
+                                            <button
+                                                disabled={true}
+                                                onClick={() => Inertia.get(`/mechanic/repairs/working/${repair.id}`)}
+                                                className="bg-yellow-500 disabled:cursor-not-allowed text-white rounded px-2 py-1 flex items-center justify-center mx-auto"
+                                            >
+                                                <AiOutlineSync className="mr-1" /> Working on
+                                            </button>
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Change Status Modal"
+                    className="m-auto w-11/12 md:w-1/2 lg:w-1/3 border border-gray-300 shadow-lg p-6 rounded-md bg-gray-900"
+                    overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex"
                 >
-                <h2 className="text-2xl text-gray-200 font-bold mb-4">Change Status</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="status">
-                            Status
-                        </label>
-                        <select
-                            id="status"
-                            value={newStatus}
-                            onChange={(e) => setNewStatus(e.target.value)}
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    <h2 className="text-2xl text-gray-200 font-bold mb-4">Change Status</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block text-gray-400 text-sm font-bold mb-2" htmlFor="status">
+                                Status
+                            </label>
+                            <select
+                                id="status"
+                                value={newStatus}
+                                onChange={(e) => setNewStatus(e.target.value)}
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            >
+                                <option value="completed">Completed</option>
+                                <option value="pending">Pending</option>
+                            </select>
+                        </div>
+                        <button
+                            type="submit"
+                            className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
                         >
-                            <option value="completed">Completed</option>
-                            <option value="pending">Pending</option>
-                        </select>
-                    </div>
-                    <button
-                        type="submit"
-                        className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
-                    >
-                        Confirm
-                    </button>
-                </form>
-            </Modal>
-                            </div>
+                            Confirm
+                        </button>
+                    </form>
+                </Modal>
+            </div>
         </div>
     );
 };
--- resources/js/Pages/Mechanic/WorkingRepairs.jsx
+++ resources/js/Pages/Mechanic/WorkingRepairs.jsx
@@ -1,44 +1,17 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-import {Card} from "flowbite-react";
-import {FaCar, FaCheckCircle} from "react-icons/fa";
+import { FaCar, FaCheckCircle } from "react-icons/fa";
 import { Inertia } from '@inertiajs/inertia';
 
-
-    export function Component(props) {
-        return (
-            <Card
-                className="max-w-sm rounded overflow-hidden shadow-lg m-4"
-                imgSrc={`/storage/${props.img}`}
-            >
-                <div className="px-6 py-4">
-                    <div className="flex items-center">
-                        <FaCar className="mr-2"/>
-                        <h5 className="font-bold text-xl mb-2 text-gray-700 dark:text-white">
-                            {props.carName}
-                        </h5>
-                    </div>
-                    <div className="flex items-center">
-                        <FaCheckCircle className="mr-2"/>
-                        <p className="text-gray-700 dark:text-gray-400 text-base">
-                            {props.status}
-                        </p>
-                    </div>
-                </div>
-            </Card>
-        );
-    }
 const RepairDetails = ({ repair, parts }) => {
-    // console.log(repair)
     const [currentRepair, setCurrentRepair] = useState(repair);
     const [currentParts, setCurrentParts] = useState(parts);
     const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 8;
 
     const addPartToRepair = (part) => {
-        console.log(part);
         Inertia.post('/mechanic/add/part-to-invoice', { part, repair });
-
-        // Add code here to add the part to the repair
     };
 
     const openModal = () => {
@@ -49,45 +22,73 @@ const RepairDetails = ({ repair, parts }) => {
         setModalIsOpen(false);
     };
 
-    return (
-        <div className="p-6">
-            <h1 className="text-3xl font-bold mb-4">Repair Details</h1>
-          <div className="mb-4 bg-white shadow-lg flex flex-col justify-center items-center rounded-lg overflow-hidden">
-    <h2 className="text-2xl font-bold mb-2 p-4">Repair Information</h2>
-    <div className="px-4 py-5 w-1/3 bg-grey-100 flex  flex-col justify-center items-center">
-        <Component img={currentRepair.vehicle_photos} carName={repair.description} status={repair.status}/>
-        <button onClick={openModal} className="bg-green-500 w-5/2 text-white px-4 py-2 rounded">Add a spare part</button>
-    </div>
-    {/*<div className="px-4 py-5 bg-gray-100">*/}
-    {/*</div>*/}
-</div>
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = currentParts.slice(indexOfFirstItem, indexOfLastItem);
 
-            {/*<h2 className="text-2xl font-bold mb-4">Spare Parts</h2>*/}
-            {/*<ul className="space-y-2">*/}
-            {/*    {currentParts.map((part) => (*/}
-            {/*        <li key={part.id} className="flex justify-between items-center border p-2 rounded">*/}
-            {/*            <span>{part.partName} - {part.price}</span>*/}
-            {/*            <button onClick={() => addPartToRepair(part)} className="bg-blue-500 text-white px-4 py-2 rounded">Add to repair</button>*/}
-            {/*        </li>*/}
-            {/*    ))}*/}
-            {/*</ul>*/}
+    const nextPage = () => {
+        setCurrentPage((prevPage) => prevPage + 1);
+    };
+
+    const prevPage = () => {
+        setCurrentPage((prevPage) => prevPage - 1);
+    };
+
+    return (
+        <div className="p-6 bg-gray-800 flex flex-col gap-4 items-center" style={{ background: '#161D32', minHeight: '100vh' }}>
+            <h1 className="text-3xl font-bold text-white mb-4">Repair Details</h1>
+            <div className="mb-4 bg-gray-900 shadow-lg rounded-lg overflow-hidden w-full max-w-4xl">
+                <h2 className="text-2xl font-bold text-white p-4">Repair Information</h2>
+                <div className="px-4 py-5 bg-gray-800 flex flex-col items-center">
+                    <div className="w-full max-w-sm rounded overflow-hidden shadow-lg mb-4">
+                        <img className="w-full" src={`/storage/${currentRepair.vehicle_photos}`} alt="Vehicle" />
+                        <div className="px-6 py-4">
+                            <div className="flex items-center">
+                                <FaCar className="mr-2 text-white" />
+                                <h5 className="font-bold text-xl mb-2 text-white">{repair.description}</h5>
+                            </div>
+                            <div className="flex items-center">
+                                <FaCheckCircle className="mr-2 text-white" />
+                                <p className="text-gray-400 text-base">{repair.status}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button onClick={openModal} className="bg-green-500 text-white px-4 py-2 rounded">Add a spare part</button>
+                </div>
+            </div>
 
             <Modal
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
                 contentLabel="Add Spare Part Modal"
-                className="m-auto w-11/12 md:w-1/2 lg:w-1/3 border border-gray-300 shadow-lg p-6 rounded-md bg-white"
-                overlayClassName="fixed inset-0 bg-black bg-opacity-20 flex"
+                className="m-auto w-500 h-90 border border-gray-300 shadow-lg p-6 rounded-md bg-gray-800"
+                overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex"
             >
-                <h2 className="text-2xl font-bold mb-4">Add Spare Part</h2>
-                <ul className="space-y-2">
-                    {currentParts.map((part) => (
-                        <li key={part.id} className="flex justify-between items-center border p-2 rounded">
-                            <span>{part.partName} - {part.price}</span>
-                            <button onClick={() => addPartToRepair(part)} className="bg-blue-500 text-white px-4 py-2 rounded">Add to repair</button>
-                        </li>
-                    ))}
-                </ul>
+                <h2 className="text-2xl font-bold mb-4 text-white">Add Spare Part</h2>
+                <table className="table-auto w-full border-collapse bg-gray-900 text-gray-400 rounded-lg">
+                    <thead>
+                        <tr className="hover:bg-gray-700 transition-colors duration-300">
+                            <th className="px-4 py-2 border-gray-300 text-center">Part Name</th>
+                            <th className="px-4 py-2 border-gray-300 text-center">Price</th>
+                            <th className="px-4 py-2 border-gray-300 text-center">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {currentItems.map((part) => (
+                            <tr key={part.id} className="hover:bg-gray-700 transition-colors duration-300">
+                                <td className="border-gray-300 px-4 py-2 text-center">{part.partName}</td>
+                                <td className="border-gray-300 px-4 py-2 text-center">{part.price}</td>
+                                <td className="border-gray-300 px-4 py-2 text-center">
+                                    <button onClick={() => addPartToRepair(part)} className="bg-blue-500 text-white px-4 py-2 rounded">Add to repair</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className="mt-4 flex justify-between">
+                    <button onClick={prevPage} disabled={currentPage === 1} className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">Previous</button>
+                    <button onClick={nextPage} disabled={currentItems.length < itemsPerPage} className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline">Next</button>
+                </div>
             </Modal>
         </div>
     );

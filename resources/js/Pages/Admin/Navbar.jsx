3509fb154ecf4bef0019d65891e843cba66a4e19
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-06-02 09:35:40 +0100

    fix notif bug

--- resources/js/Pages/Admin/Navbar.jsx
+++ resources/js/Pages/Admin/Navbar.jsx
@@ -15,6 +15,54 @@ import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
+function Modal({ messages, onClose }) {
+    console.log(messages)
+    return (
+        <div className="fixed z-50 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true" onClick={onClose}></div>
+                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div className="inline-block align-bottom bg-gray-900 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                    <div className="bg-gray-900  text-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+
+                        <div className="sm:flex sm:items-start">
+
+                            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+
+                                <h3 className="text-lg  leading-6 font-medium text-white" id="modal-title">
+                                    Notifications
+                                </h3>
+                                <div className="mt-2">
+                                    {messages && messages.length > 0 ? (
+                                        messages.map((message, index) => (
+                                            <div key={index}>
+                                                <p className="text-sm text-gray-500">
+                                                    {message.content}
+                                                </p>
+                                                {index < messages.length - 1 && <hr />}
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <p className="text-sm text-gray-500">
+                                            No notifications
+                                        </p>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-gray-900 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={onClose}>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 function NavBar({ auth, notifications }) {
     const [isModalVisible, setModalVisible] = useState(false);
     const dsh = '/ndashboard/' + auth.id;

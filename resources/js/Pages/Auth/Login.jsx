7608c24ceefd0d1938eada5805cdca90a51336df
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-06-02 20:39:48 +0100

    login link to register

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -127,7 +127,7 @@ export default function Login({ status, canResetPassword }) {
                             </div>
                             <button type="submit" disabled={processing} onClick={submit} class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="#" class="font-medium text-blue-500 hover:underline dark:text-primary-500">Sign up</a>
+                                Don’t have an account yet? <a href="/register" class="font-medium text-blue-500 hover:underline dark:text-primary-500">Sign up</a>
                             </p>
                         </form>
                     </div>

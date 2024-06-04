58dc7e6dfd468665eeaeeb3216dfe3ffab0c4122
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-06-04 15:31:02 +0100

    Fix capitalization in success message

--- resources/js/Pages/repairs/SuccessRequest.jsx
+++ resources/js/Pages/repairs/SuccessRequest.jsx
@@ -9,7 +9,7 @@ export default function SuccessRequest({ auth , notifications}){
         <NavBar auth={auth} notifications={notifications}/>
     <div className="flex items-center justify-center flex-col gap-4 h-screen bg-gray-800">
         <div className="p-5 text-lg font-bold text-center text-green-500 bg-gray-800 border-4 border-gray-700 rounded-2xl shadow-lg">
-            Your request has been made and you will recive an email soon with start date and the end date of your repair
+            Your request has been made and you will Receive an email soon with start date and the end date of your repair
         </div>
         <button className="p-3 text-lg font-bold text-center text-green-500 bg-gray-800 border-4 border-gray-700 rounded-2xl shadow-lg"><a href={dsh}> go back to home page </a></button>
     </div>

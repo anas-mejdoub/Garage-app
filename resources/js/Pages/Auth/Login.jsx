ec5b9936f0a7ea9978702f5b1f64945c044986e3
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-06-04 16:53:52 +0100

    Fix bug in login functionality

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -31,9 +31,9 @@ export default function Login({ status, canResetPassword }) {
         // <GuestLayout>
         //     <Head title="Log in" />
 
-        //     {status && <div className="mb-4 bg-gray-700 font-medium text-sm text-green-600">{status}</div>}
+        //     {status && <div classNameName="mb-4 bg-gray-700 font-medium text-sm text-green-600">{status}</div>}
 
-        //     <form onSubmit={submit} className='bg-gray-700'>
+        //     <form onSubmit={submit} classNameName='bg-gray-700'>
         //         <div>
         //             <InputLabel htmlFor="email" value="Email" />
 
@@ -42,16 +42,16 @@ export default function Login({ status, canResetPassword }) {
         //                 type="email"
         //                 name="email"
         //                 value={data.email}
-        //                 className="mt-1 block w-full"
+        //                 classNameName="mt-1 block w-full"
         //                 autoComplete="username"
         //                 isFocused={true}
         //                 onChange={(e) => setData('email', e.target.value)}
         //             />
 
-        //             <InputError message={errors.email} className="mt-2" />
+        //             <InputError message={errors.email} classNameName="mt-2" />
         //         </div>
 
-        //         <div className="mt-4">
+        //         <div classNameName="mt-4">
         //             <InputLabel htmlFor="password" value="Password" />
 
         //             <TextInput
@@ -59,68 +59,68 @@ export default function Login({ status, canResetPassword }) {
         //                 type="password"
         //                 name="password"
         //                 value={data.password}
-        //                 className="mt-1 block w-full"
+        //                 classNameName="mt-1 block w-full"
         //                 autoComplete="current-password"
         //                 onChange={(e) => setData('password', e.target.value)}
         //             />
 
-        //             <InputError message={errors.password} className="mt-2" />
+        //             <InputError message={errors.password} classNameName="mt-2" />
         //         </div>
 
-        //         <div className="block mt-4">
-        //             <label className="flex items-center">
+        //         <div classNameName="block mt-4">
+        //             <label classNameName="flex items-center">
         //                 <Checkbox
         //                     name="remember"
         //                     checked={data.remember}
         //                     onChange={(e) => setData('remember', e.target.checked)}
         //                 />
-        //                 <span className="ms-2 text-sm text-gray-600">Remember me</span>
+        //                 <span classNameName="ms-2 text-sm text-gray-600">Remember me</span>
         //             </label>
         //         </div>
 
-        //         <div className="flex items-center justify-end mt-4">
+        //         <div classNameName="flex items-center justify-end mt-4">
         //             {canResetPassword && (
         //                 <Link
         //                     href={route('password.request')}
-        //                     className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        //                     classNameName="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         //                 >
         //                     Forgot your password?
         //                 </Link>
         //             )}
 
-        //             <PrimaryButton className="ms-4" disabled={processing}>
+        //             <PrimaryButton classNameName="ms-4" disabled={processing}>
         //                 Log in
         //             </PrimaryButton>
         //         </div>
         //     </form>
         // </GuestLayout>
-        <section class="bg-gray-50 dark:bg-gray-900">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img class="w-18 h-10 mr-2" src={Logo} alt="logo" />
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <img className="w-18 h-10 mr-2" src={Logo} alt="logo" />
                     Garagiste
                 </a>
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Sign in to your account
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6" action="#">
                             <div>
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" value={data.email} onChange={(e) => setData('email', e.target.value)} name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input type="email" value={data.email} onChange={(e) => setData('email', e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
                             </div>
                             <div>
-                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" value={data.password} onChange={(e) => setData('password', e.target.value)} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" value={data.password} onChange={(e) => setData('password', e.target.value)} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
-                            <div class="flex items-center justify-between">
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                        <input id="remember" checked={data.remember} onChange={(e) => setData('remember', e.target.checked)}aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input id="remember" checked={data.remember} onChange={(e) => setData('remember', e.target.checked)} aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
                                     </div>
-                                    <div class="ml-3 text-sm">
-                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                    <div className="ml-3 text-sm">
+                                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
                                     </div>
                                 </div>
                                 <Link
@@ -130,9 +130,9 @@ export default function Login({ status, canResetPassword }) {
                             Forgot your password?
                         </Link>
                             </div>
-                            <button type="submit" disabled={processing} onClick={submit} class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="/register" class="font-medium text-blue-500 hover:underline dark:text-primary-500">Sign up</a>
+                            <button type="submit" disabled={processing} onClick={submit} className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Don’t have an account yet? <a href="/register" className="font-medium text-blue-500 hover:underline dark:text-primary-500">Sign up</a>
                             </p>
                         </form>
                     </div>
